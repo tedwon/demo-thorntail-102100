2f7fc163d5666a27f813dd301048a98f86129472
tedwon <jwon@redhat.com>
2018-10-21 01:23:11 +0900

    jx

---
+++ src/main/java/com/example/demothorntail102100/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package com.example.demothorntail102100.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
